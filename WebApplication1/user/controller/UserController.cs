305ce1570a7ce1adc70306f41f3a040b5a2661e3
BL <109878606+Bartlomiej-Lipinski@users.noreply.github.com>
2025-07-30 15:49:11 +0200

    Implement user creation and retrieval endpoints in UserController

--- WebApplication1/user/controller/UserController.cs
+++ WebApplication1/user/controller/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.user.dto;
 using WebApplication1.user.service;
 
 namespace WebApplication1.user.controller;
@@ -7,5 +8,37 @@ namespace WebApplication1.user.controller;
 [Route("api/[controller]")]
 public class UserController(IUserService userService) : ControllerBase
 {
-   
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateUserAsync([FromBody] UserRequestDto request)
+    {
+        if (request == null)
+        {
+            return BadRequest("Request cannot be null.");
+        }
+
+        var result = await userService.CreateUserAsync(request);
+        if (result == null)
+        {
+            return BadRequest("Failed to create user.");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("get/{user}")]
+    public async Task<IActionResult> GetUserAsync(string user)
+    {
+        if (string.IsNullOrEmpty(user))
+        {
+            return BadRequest("User name cannot be null or empty.");
+        }
+
+        var result = await userService.GetUserAsync(user);
+        if (result == null)
+        {
+            return NotFound("User not found.");
+        }
+
+        return Ok(result);
+    }
 }
--- WebApplication1/user/service/UserService.cs
+++ WebApplication1/user/service/UserService.cs
@@ -1,8 +1,9 @@
-﻿using WebApplication1.user.dto;
+﻿using WebApplication1.context;
+using WebApplication1.user.dto;
 
 namespace WebApplication1.user.service;
 
-public class UserService : IUserService
+public class UserService(AppDbContext context) : IUserService
 {
     public Task<bool> ValidateUserAsync(string userName, string password)
     {
@@ -11,7 +12,25 @@ public class UserService : IUserService
 
     public Task<UserRequestDto> CreateUserAsync(UserRequestDto request)
     {
-        throw new NotImplementedException();
+        var userRequest = new UserRequestDto()
+        {
+            Name = request.Name,
+            Surname = request.Surname,
+            Email = request.Email,
+            BirthDate = request.BirthDate,
+            Password = request.Password
+        };
+        var emailExists = context.Users.Any(u => u.Email == userRequest.Email);
+        if (emailExists)
+        {
+            throw new ArgumentException("Email already exists.");
+        }
+        if (string.IsNullOrEmpty(request.Name) || string.IsNullOrEmpty(request.Surname) || string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Password))
+        {
+            throw new ArgumentException("All fields are required.");
+        }
+
+        return Task.FromResult(userRequest);
     }
 
     public Task<UserResponseDto> GetUserAsync(string userName)
