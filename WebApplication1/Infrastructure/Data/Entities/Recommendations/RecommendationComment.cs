0bd81acd9bffaec235971d648851604c2a5f00d0
Weronika Wydra <weronika.wydra15@gmail.com>
2025-10-15 15:54:47 +0200

    Code review fix

--- WebApplication1/Infrastructure/Data/Entities/Recommendations/RecommendationComment.cs
+++ WebApplication1/Infrastructure/Data/Entities/Recommendations/RecommendationComment.cs
@@ -2,7 +2,7 @@
 
 public class RecommendationComment
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; } = null!;
     
     public string RecommendationId { get; set; } = null!;
     
