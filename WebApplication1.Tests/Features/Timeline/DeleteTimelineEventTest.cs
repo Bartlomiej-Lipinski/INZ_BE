46dadba5a3b416e3d20e0025f3bfdb452d139e9e
Weronika Wydra <weronika.wydra15@gmail.com>
2025-11-06 10:14:48 +0100

    Quick commit

---
+++ WebApplication1.Tests/Features/Timeline/DeleteTimelineEventTest.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Tests.Features.Timeline;
+
+public class DeleteTimelineEventTest : TestBase
+{
+    
+}
---
+++ WebApplication1/Features/Timeline/DeleteTimelineEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Infrastructure.Data.Context;
+using WebApplication1.Shared.Endpoints;
+
+namespace WebApplication1.Features.Timeline;
+
+// public class DeleteTimelineEvent : IEndpoint
+// {
+//     public void RegisterEndpoint(IEndpointRouteBuilder app)
+//     {
+//         app.MapDelete("/groups/{groupId}/timeline/{eventId}", Handle)
+//             .WithName("DeleteTimelineEvent")
+//             .WithDescription("Deletes a timeline event in a specific group")
+//             .WithTags("Timeline")
+//             .RequireAuthorization();
+//     }
+//
+//     public static async Task<IResult> Handle(
+//         [FromRoute] string groupId,
+//         [FromRoute] string eventId,
+//         AppDbContext dbContext,
+//         ClaimsPrincipal currentUser,
+//         HttpContext httpContext,
+//         ILogger<DeleteTimelineEvent> logger,
+//         CancellationToken cancellationToken)
+//     {
+//         
+//     }
+// }
